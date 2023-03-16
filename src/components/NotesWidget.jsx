3ee605298ab370fbe39406491fd393d20f300b9f
Bethany Ryalls <bethany@motifdesignstudio.com>
2023-03-16 21:01:41 +0000

    adding basic structure to NotesWidget

--- src/components/NotesWidget.jsx
+++ src/components/NotesWidget.jsx
@@ -1,22 +1,35 @@
-import { Card, Button} from 'react-bootstrap'
+import { useQuill } from 'react-quilljs';
+import 'quill/dist/quill.snow.css';
+import React, { useState } from 'react';
 
+const Notes = ({ note }) => {
+  return (
+    <div>
+      <h2>Note:</h2>
+      <div>{note}</div>
+    </div>
+  );
+};
 
 const NotesWidget = () => {
-    return (
-        <div>
-        <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
-      <Card.Body>
-        <Card.Title>Notes Widget</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+  const { quill, quillRef } = useQuill();
+  const [noteValue, setNoteValue] = useState('');
+
+  const handleSaveButtonClick = () => {
+    let saveNote = quillRef.current.firstChild.innerHTML;
+    setNoteValue(saveNote);
+  };
+  
+
+  return (
+    <div>
+      <div style={{ width: 500, height: 300 }}>
+        <div ref={quillRef} />
+      </div>
+      <button onClick={handleSaveButtonClick}>Save</button>
+      {noteValue && <Notes note={noteValue} />}
     </div>
-    );
+  );
 };
 
 export default NotesWidget;
