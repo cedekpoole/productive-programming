3c35bd6b705967da26a573d53833e87635e993c1
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-03-16 19:14:58 +0000

    place header in app component and link navbar using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import './App.css'
+
+import { Routes, Route } from "react-router-dom";
+
+import Header from './components/Header'
+import News from './pages/News'
+import Notes from './pages/Notes'
+import StackOverflow from './pages/StackOverflow'
+import Study from './pages/Study'
 import Home from './pages/Home'
 
 function App() {
 
   return (
     <div className="App">
-      <Home />
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/study" element={<Study />} />
+        <Route path="/stack-overflow" element={<StackOverflow />} />
+      </Routes>
     </div>
   )
 }
 
-export default App
+export default App;
