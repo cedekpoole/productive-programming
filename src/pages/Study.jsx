fad863636aa19a94393434e6910a8609fc239452
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-03-20 15:36:55 +0000

    restyle study page to fix theme of application

--- src/pages/Study.jsx
+++ src/pages/Study.jsx
@@ -1,10 +1,10 @@
 import Timer from "../components/TimerWidget";
-import Astronomy from "../components/AstronomyAPI"
-import { useState, useEffect } from "react";
-import { GrLounge } from "react-icons/gr";
+import Astronomy from "../utils/AstronomyAPI";
+import { useState } from "react";
 import { BsFillPlayCircleFill } from "react-icons/bs";
 import { BsPauseCircleFill } from "react-icons/bs";
 import { Container, Row, Col, Card } from "react-bootstrap";
+import Quote from '../utils/ZenQuotesAPI';
 
 import { Howl } from "howler";
 import studyMP3 from "../media/study-music.mp3";
@@ -21,37 +21,44 @@ const Study = () => {
   playMusic ? studyMusic.play() : studyMusic.pause();
 
   return (
-    <div className="text-center">
-      <h1 className="display-1 text-light mb-4">
-        Study Lounge <GrLounge />
-      </h1>
-      <Container fluid>
-        <Row>
-          <Col
-            xs={12}
-            md={5}
-            className="d-flex align-items-center flex-column mb-2"
-          >
-            <Timer />
-            <Card className="mt-4 bg-dark text-light">
-              <Card.Body>
-                <Card.Title className="mb-0">Music to Focus</Card.Title>
-                <Card.Text style={{ fontSize: "2rem" }}>
-                  <span onClick={toggleMusic}>{playMusic ? (
-                    <BsPauseCircleFill role="button" />
-                  ) : (
-                    <BsFillPlayCircleFill role="button" />
-                  )}</span>
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-          <Col xs={12} md={7} className="d-flex justify-content-center">
-            <Astronomy />
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <Container>
+      <h1 className="display-3 text-light mb-3 mt-2">Study Lounge</h1>
+      <hr className="text-light"></hr>
+      <Row className="text-center">
+        <Col xs={12} md={5} className="mb-2 border-end border-white">
+          <Row>
+            <Col className="mb-3 d-flex justify-content-center">
+              <Timer />
+            </Col>
+            <Col className="d-flex justify-content-center">
+              <div className="glass" style={{ width: "18rem" }}>
+                <Card
+                  className=" text-light p-1 bg-transparent border-0"
+                  style={{ width: "18rem" }}
+                >
+                  <Card.Body>
+                    <Card.Title className="mb-0">Music to Focus</Card.Title>
+                    <Card.Text style={{ fontSize: "2rem" }}>
+                      <span onClick={toggleMusic}>
+                        {playMusic ? (
+                          <BsPauseCircleFill role="button" />
+                        ) : (
+                          <BsFillPlayCircleFill role="button" />
+                        )}
+                      </span>
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+        <Col xs={12} md={7} className="d-flex justify-content-center">
+          <Astronomy />
+        </Col>
+      </Row>
+      <Quote />
+    </Container>
   );
 };
 
