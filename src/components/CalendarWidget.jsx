c990f8624160edbb273e4c13f3bcc42b8b4d40eb
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-03-22 20:16:36 +0000

    fix time component

--- src/components/CalendarWidget.jsx
+++ src/components/CalendarWidget.jsx
@@ -1,23 +1,29 @@
 import React, { useState, useEffect } from "react";
+import { compareAsc, format } from 'date-fns'   
 
 const CalendarWidget = () => {
-  var [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(new Date());
 
   useEffect(() => {
-    const timer = setInterval(() => setDate(new Date()), 1000);
+    const timer = setInterval(() => { 
+      setDate(format(new Date(), `kk:mm:ss MM/dd/yyyy`))
+    }, 1000);
     return () => clearInterval(timer);
   });
 
+  const dateString = JSON.stringify(date).substring(1, JSON.stringify(date).length -1 )
+  const dateArr = dateString.split(' ')
+ 
+
   return (
     <div className="text-center glass p-3 d-flex flex-column align-items-center" style={{width: "17rem"}}>
-      {/* <p className="border rounded p-2 text-light">{date.toLocaleDateString()}</p> */}
       <p
         className="mb-0"
         style={{fontSize: "4rem"}}
       >
-        {date.toLocaleTimeString()}
+        {dateArr[0]}
       </p>
-      <p>{date.toLocaleDateString()}</p>
+      <p>{dateArr[1]}</p>
     </div>
   );
 };
