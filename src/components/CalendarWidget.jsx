e6fbb3cab0dc05dd7d9ae0b9a525251e5a0475ff
Bethany Ryalls <bethany@motifdesignstudio.com>
2023-03-22 20:15:39 +0000

    fixing flex issue where calendar widget wasn't centered

--- src/components/CalendarWidget.jsx
+++ src/components/CalendarWidget.jsx
@@ -12,12 +12,12 @@ const CalendarWidget = () => {
     <div className="text-center glass p-3 d-flex flex-column align-items-center" style={{height: "100%"}}>
       {/* <p className="border rounded p-2 text-light">{date.toLocaleDateString()}</p> */}
       <p
-        className="mb-0"
+        className="mt-auto mb-0"
         style={{fontSize: "3rem"}}
       >
         {date.toLocaleTimeString()}
       </p>
-      <p>{date.toLocaleDateString()}</p>
+      <p className="mb-auto">{date.toLocaleDateString()}</p>
     </div>
   );
 };
