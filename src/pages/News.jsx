b472096ef542c9eb3c5c7cff5600d4da76e85d93
xarli <117117311+c4rli@users.noreply.github.com>
2023-03-23 14:31:01 +0000

    comment and cleanup News.jsx

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Container, Card, Button, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import { FaBriefcase, FaStar, FaCommentMedical, FaUserTie, FaVolleyballBall, FaMapMarkedAlt, FaGlobeEurope, FaAtom } from "react-icons/fa";
 import axios from 'axios';
 import '../components/NewsWidget/newsPage.css';
@@ -22,20 +22,22 @@ const News = () => {
     const categoryList = [
         { name: 'Business', icon: <FaBriefcase /> },
         { name: 'Entertainment', icon: <FaStar /> },
-        { name: 'Health', icon: <FaCommentMedical />},
-        { name: 'Politics', icon: <FaUserTie />},
-        { name: 'Sports', icon: <FaVolleyballBall />},
-        { name: 'UK', icon: <FaMapMarkedAlt />},
-        { name: 'World', icon: <FaGlobeEurope />},
-        { name: 'ScienceAndTechnology', icon: <FaAtom />},
+        { name: 'Health', icon: <FaCommentMedical /> },
+        { name: 'Politics', icon: <FaUserTie /> },
+        { name: 'Sports', icon: <FaVolleyballBall /> },
+        { name: 'UK', icon: <FaMapMarkedAlt /> },
+        { name: 'World', icon: <FaGlobeEurope /> },
+        { name: 'ScienceAndTechnology', icon: <FaAtom /> },
     ];
 
+    // Increases apiFailCounter by 1 and then waits 1 second 
     const apiCallFail = () => {
         setTimeout(() => {
             setAPIFailCounter(apiFailCounter + 1);
         }, 1000);
     };
 
+    // object containing the request options including method, URL, and headers.
     useEffect(() => {
         if (apiFailCounter < 3) {
             const options = {
@@ -58,6 +60,9 @@ const News = () => {
                 }
             };
 
+            // axios request makes call using predetermined options.
+            // If the search results page is greater than 0, append new search results to the existing ones. 
+            // Else only display new results.
             axios.request(options)
                 .then(function (response) {
                     if (searchResultsPage > 0) {
@@ -67,15 +72,18 @@ const News = () => {
                     }
                 })
                 .catch(function (error) {
+                    // call apiCallFail on failed request
                     apiCallFail(error);
                 });
         }
     }, [searchResultsPage, category, queryString, apiFailCounter]);
 
+    // Increments the search results page by 12 to display more news articles.
     function loadMoreNews() {
         setSearchResultsPage(searchResultsPage + 12);
     }
 
+    // Sets the search type to "news/search", clears the category and search results page, sets the page title to the query, and sets the query string to the search input.
     function searchKeywords(query) {
         setSearchType("news/search");
         setCategory("");
@@ -84,6 +92,7 @@ const News = () => {
         setQueryString(query);
     }
 
+    // Sets the search type to "news", clears the search string and search results page, sets the page title to the category, and sets the category to the button value.
     function searchCategories(category) {
         setSearchType("news");
         setSearchString("");
@@ -92,15 +101,18 @@ const News = () => {
         setCategory(category);
     }
 
+    // Sets the search string to the value of the search input.
     const handleInputChange = (event) => {
         setSearchString(event.target.value);
     };
 
+    // Triggers the searchKeywords function passing in the search string as a parameter.
     const handleFormSubmit = (event) => {
         event.preventDefault();
         searchKeywords(searchString);
     };
 
+    // Triggers the searchKeywords function if the Enter key is pressed while in the search input.
     const handleKeyDown = (event) => {
         if (event.key === "Enter") {
             event.preventDefault();
@@ -108,11 +120,13 @@ const News = () => {
         }
     };
 
+    // Triggers the searchCategories function passing in the button value as a parameter.
     const handleCategorySearch = (event) => {
         event.preventDefault();
         searchCategories(event.currentTarget.value);
     };
 
+    // Return news page
     return (
         <Container className="news-container">
             <div className="notes__header my-3"><h1 className="notes__title">News - {pageTitle}</h1></div>
